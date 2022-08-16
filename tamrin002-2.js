3eff90d5fd6f5cdd91e452f84df66ee1a4cc713b
ahmadmohajer <110981673+ahmadmohajer@users.noreply.github.com>
2022-08-16 11:22:04 +0430

    Add files via upload

---
+++ tamrin002-2.js
@@ -0,0 +1,17 @@
+let sum = 0;
+for (i = 0; i < 1000; i++) {
+
+    let num = Number(prompt("enter your number"));
+    sum = sum + num
+    if (num == 0) {
+        console.log(sum)
+        break;
+
+        
+
+
+    }
+
+
+
+}
